ed412d837f38a58a4894f7d5d4c35caa7e353196
ForeverFancy <1026816510@qq.com>
2020-06-08 00:27:27 +0800

    [code] support global padding

--- layers.py
+++ layers.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 
 class SubGraphLayer(nn.Module):
     def __init__(self, in_features: int = 7, hidden_size: int = 64):
+        '''
+        Layer of subgraph
+        '''
         super(SubGraphLayer, self).__init__()
         self.hidden_size = hidden_size
         self.in_features = in_features
@@ -18,26 +21,30 @@ class SubGraphLayer(nn.Module):
         '''
         TODO: input size not fixed, do padding?
 
-        @input x of shape(num_of_seqs, max_seq_size, in_features)
+        @input x of shape (batch_size, num_of_seqs, max_seq_size, in_features)
 
-        @return out of shape(num_of_seqs, max_seq_size, hidden_size * 2): output features
+        @input mask of shape (batch_size, num_of_seqs, max_seq_size)
+
+        @return out of shape (batch_size, num_of_seqs, max_seq_size, hidden_size * 2): output features
         '''
-        # x now is shape(*, hidden_size)
         x = self.encode(x)
         # print(x.shape)
-        ag = self.aggregate(x).repeat(1, x.shape[1], 1)
+        ag = self.aggregate(x).repeat(1, 1, x.shape[2], 1)
         # print(ag.shape)
-        out = torch.cat([x, ag], dim=2)
-        # print(out.shape)
-        out = out * mask.unsqueeze(dim=-1).repeat(1, 1, self.hidden_size * 2)
-        assert out.shape == (x.shape[0], x.shape[1], self.hidden_size * 2)
+        out = torch.cat([x, ag], dim=3)
+        assert out.shape == (x.shape[0], x.shape[1], x.shape[2], self.hidden_size * 2)
+        out = out * (mask.unsqueeze(dim=-1).repeat(1, 1, 1, self.hidden_size * 2))
+        assert out.shape == (x.shape[0], x.shape[1], x.shape[2], self.hidden_size * 2)
+
         return out
 
     def encode(self, x: torch.Tensor) -> torch.Tensor:
         '''
-        @input x of shape(num_of_seqs, max_seq_size, in_features)
+        MLP + layer normalization + relu activation
 
-        @return x of shape(num_of_seqs, max_seq_size, hidden_size)
+        @input x of shape (batch_size, num_of_seqs, max_seq_size, in_features)
+
+        @return x of shape (batch_size, num_of_seqs, max_seq_size, hidden_size)
         '''
         x = self.linear(x)
         x = self.lm(x)
@@ -45,10 +52,13 @@ class SubGraphLayer(nn.Module):
 
     def aggregate(self, x: torch.Tensor) -> torch.Tensor:
         '''
-        @input x of shape(num_of_seqs, max_seq_size, hidden_size)
+        Use maxpooling to aggregate
 
-        @return x of shape(num_of_seqs, hidden_size)
+        @input x of shape (batch_size, num_of_seqs, max_seq_size, hidden_size)
+
+        @return x of shape (batch_size, num_of_seqs, hidden_size)
         '''
         # TODO: change dim if input shape is not (*, hidden_size)
-        y, _ = torch.max(x, dim=1)
-        return y.unsqueeze(1)
+        y, _ = torch.max(x, dim=2)
+        # print(y.unsqueeze(2).shape)
+        return y.unsqueeze(2)
--- model.py
+++ model.py
@@ -1,10 +1,14 @@
 import torch
 import torch.nn as nn
 from layers import SubGraphLayer
+import numpy as np
 
 
 class SubGraph(nn.Module):
     def __init__(self, in_features: int = 7, hidden_size: int = 64):
+        '''
+        Subgraph model
+        '''
         super(SubGraph, self).__init__()
         self.hidden_size = hidden_size
         self.in_features = in_features
@@ -19,11 +23,11 @@ class SubGraph(nn.Module):
         '''
         TODO: input size not fixed, do padding?
 
-        @input x of shape(num_of_seqs, max_seq_size, in_features)
+        @input x of shape(batch_size, num_of_seqs, max_seq_size, in_features)
 
-        @input mask of shape(num_of_seqs, max_seq_size, in_features)
+        @input mask of shape(batch_size, num_of_seqs, max_seq_size, in_features)
 
-        @return out of shape(num_of_seqs, hidden_size * 2): polyline level feature
+        @return out of shape(batch_size, num_of_seqs, hidden_size * 2): polyline level feature
         '''
         # x now is shape(*, hidden_size)
         x = self.sglayer1(x, mask)
@@ -34,46 +38,64 @@ class SubGraph(nn.Module):
 
     def aggregate(self, x: torch.Tensor) -> torch.Tensor:
         '''
-        @input x of shape(num_of_seqs, max_seq_size, hidden_size * 2)
+        @input x of shape(batch_size, num_of_seqs, max_seq_size, hidden_size * 2)
 
-        @return x of shape(num_of_seqs, hidden_size * 2)
+        @return x of shape(batch_size, num_of_seqs, hidden_size * 2)
         '''
         # TODO: change dim if input shape is not (*, hidden_size)
-        y, _ = torch.max(x, dim=1)
+        y, _ = torch.max(x, dim=2)
+        # print(y.shape)
         return y
 
 
 class GlobalGraph(nn.Module):
-    def __init__(self,):
+    def __init__(self, in_features: int = 128, out_features: int = 128):
+        '''
+        Global graph model
+        '''
         super(GlobalGraph, self).__init__()
-        self.Proj_Q = nn.Linear(in_features=128, out_features=128, bias=False)
-        self.Proj_K = nn.Linear(in_features=128, out_features=128, bias=False)
-        self.Proj_V = nn.Linear(in_features=128, out_features=128, bias=False)
+        self.Proj_Q = nn.Linear(in_features=in_features, out_features=out_features, bias=False)
+        self.Proj_K = nn.Linear(in_features=in_features, out_features=out_features, bias=False)
+        self.Proj_V = nn.Linear(in_features=in_features, out_features=out_features, bias=False)
+        self.softmax = nn.Softmax(dim=2)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, query: torch.Tensor, x: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
         '''
-        @input x of shape(num_of_nodes, 2 * hidden_size): polyline node features
+        Do self-attention
 
-        @return out of shape(num_of_nodes, 2 * hidden_size): self-attention output
+        @input query of shape(batch_size, 1, 2 * hidden_size): agent features
+
+        @input x of shape (batch_size, num_of_nodes, 2 * hidden_size): all polyline node features
+
+        @input attention_mask of shape (batch_size, num_of_nodes): mask for self attention
+
+        @return out of shape(batch_size, num_of_nodes, 2 * hidden_size): self-attention output
         '''
-        P_q = self.Proj_Q(x)
+        P_q = self.Proj_Q(query)
         P_k = self.Proj_K(x)
         P_v = self.Proj_V(x)
-        # print(P_q.shape, P_k.shape)
-        out = torch.matmul((torch.matmul(P_q, P_k.t())).softmax(dim=1), P_v)
+        # print(attention_mask, attention_mask.shape)
+        out = torch.bmm(P_q, P_k.transpose(1, 2))
+        print(out.shape, attention_mask.unsqueeze(1).expand(-1, query.shape[1], -1).shape)
+        # mask for self attention
+        out = out.masked_fill(attention_mask.unsqueeze(1).expand(-1, query.shape[1], -1) == 0, -1e9)
+        out = torch.bmm(self.softmax(out), P_v)
         return out
 
 
 class TrajectoryDecoder(nn.Module):
-    def __init__(self, out_features: int = 2):
+    def __init__(self, in_features: int = 128, out_features: int = 2):
+        '''
+        Decode future trajectory
+        '''
         super(TrajectoryDecoder, self).__init__()
-        self.linear = nn.Linear(in_features=128, out_features=2, bias=True)
+        self.linear = nn.Linear(in_features=in_features, out_features=out_features, bias=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         '''
-        @input x of shape(*, 128)
+        @input x of shape(*, in_features)
 
-        @return out of shape(*, 2)
+        @return out of shape(*, out_features)
         '''
         out = self.linear(x)
         return out
